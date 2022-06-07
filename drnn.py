20a78bd5827e33be11865aeebe373504e6544b05
lcy.seso <lcy.seso@gmail.com>
2022-06-07 13:37:19 +0800

    debug.

--- drnn.py
+++ drnn.py
@@ -1,16 +1,23 @@
 import torch
 import torch.nn as nn
 
-
 use_cuda = torch.cuda.is_available()
+
+import pdb
 
 
 class DRNN(nn.Module):
-
-    def __init__(self, n_input, n_hidden, n_layers, dropout=0, cell_type='GRU', batch_first=False):
+    def __init__(self,
+                 n_input,
+                 n_hidden,
+                 n_layers,
+                 dropout=0,
+                 cell_type='GRU',
+                 batch_first=False):
         super(DRNN, self).__init__()
 
-        self.dilations = [2 ** i for i in range(n_layers)]
+        self.dilations = [2**i for i in range(n_layers)]
+
         self.cell_type = cell_type
         self.batch_first = batch_first
 
@@ -40,7 +47,8 @@ class DRNN(nn.Module):
             if hidden is None:
                 inputs, _ = self.drnn_layer(cell, inputs, dilation)
             else:
-                inputs, hidden[i] = self.drnn_layer(cell, inputs, dilation, hidden[i])
+                inputs, hidden[i] = self.drnn_layer(cell, inputs, dilation,
+                                                    hidden[i])
 
             outputs.append(inputs[-dilation:])
 
@@ -57,23 +65,37 @@ class DRNN(nn.Module):
         dilated_inputs = self._prepare_inputs(inputs, rate)
 
         if hidden is None:
-            dilated_outputs, hidden = self._apply_cell(dilated_inputs, cell, batch_size, rate, hidden_size)
+            dilated_outputs, hidden = self._apply_cell(
+                dilated_inputs, cell, batch_size, rate, hidden_size)
         else:
             hidden = self._prepare_inputs(hidden, rate)
-            dilated_outputs, hidden = self._apply_cell(dilated_inputs, cell, batch_size, rate, hidden_size, hidden=hidden)
+            dilated_outputs, hidden = self._apply_cell(
+                dilated_inputs,
+                cell,
+                batch_size,
+                rate,
+                hidden_size,
+                hidden=hidden)
 
         splitted_outputs = self._split_outputs(dilated_outputs, rate)
         outputs = self._unpad_outputs(splitted_outputs, n_steps)
 
         return outputs, hidden
 
-    def _apply_cell(self, dilated_inputs, cell, batch_size, rate, hidden_size, hidden=None):
+    def _apply_cell(self,
+                    dilated_inputs,
+                    cell,
+                    batch_size,
+                    rate,
+                    hidden_size,
+                    hidden=None):
         if hidden is None:
             if self.cell_type == 'LSTM':
                 c, m = self.init_hidden(batch_size * rate, hidden_size)
                 hidden = (c.unsqueeze(0), m.unsqueeze(0))
             else:
-                hidden = self.init_hidden(batch_size * rate, hidden_size).unsqueeze(0)
+                hidden = self.init_hidden(batch_size * rate,
+                                          hidden_size).unsqueeze(0)
 
         dilated_outputs, hidden = cell(dilated_inputs, hidden)
 
@@ -85,12 +107,14 @@ class DRNN(nn.Module):
     def _split_outputs(self, dilated_outputs, rate):
         batchsize = dilated_outputs.size(1) // rate
 
-        blocks = [dilated_outputs[:, i * batchsize: (i + 1) * batchsize, :] for i in range(rate)]
+        blocks = [
+            dilated_outputs[:, i * batchsize:(i + 1) * batchsize, :]
+            for i in range(rate)
+        ]
 
         interleaved = torch.stack((blocks)).transpose(1, 0).contiguous()
-        interleaved = interleaved.view(dilated_outputs.size(0) * rate,
-                                       batchsize,
-                                       dilated_outputs.size(2))
+        interleaved = interleaved.view(
+            dilated_outputs.size(0) * rate, batchsize, dilated_outputs.size(2))
         return interleaved
 
     def _pad_inputs(self, inputs, n_steps, rate):
@@ -100,8 +124,7 @@ class DRNN(nn.Module):
             dilated_steps = n_steps // rate + 1
 
             zeros_ = torch.zeros(dilated_steps * rate - inputs.size(0),
-                                 inputs.size(1),
-                                 inputs.size(2))
+                                 inputs.size(1), inputs.size(2))
             if use_cuda:
                 zeros_ = zeros_.cuda()
 
@@ -112,7 +135,8 @@ class DRNN(nn.Module):
         return inputs, dilated_steps
 
     def _prepare_inputs(self, inputs, rate):
-        dilated_inputs = torch.cat([inputs[j::rate, :, :] for j in range(rate)], 1)
+        dilated_inputs = torch.cat(
+            [inputs[j::rate, :, :] for j in range(rate)], 1)
         return dilated_inputs
 
     def init_hidden(self, batch_size, hidden_dim):
--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import unittest
 import drnn
 import torch
@@ -5,59 +7,65 @@ import torch
 
 class TestForward(unittest.TestCase):
     def test(self):
-        model = drnn.DRNN(10, 10, 4, 0, 'GRU')
+        model = drnn.DRNN(
+            n_input=10,
+            n_hidden=10,
+            n_layers=4,
+            dropout=0,
+            cell_type='GRU',
+            batch_first=False).to("cuda:0")
 
-        x = torch.randn(23, 3, 10)
-        out = model(x)[0]
+        x = torch.randn(23, 3, 10, device="cuda:0")
+        out = model(x)
 
-        self.assertTrue(out.size(0) == 23)
-        self.assertTrue(out.size(1) == 3)
-        self.assertTrue(out.size(2) == 10)
-
-
-class TestReshape(unittest.TestCase):
-    def test(self):
-        model = drnn.DRNN(10, 10, 4, 0, 'GRU')
-
-        x = torch.randn(24, 3, 10)
-
-        split_x = model._prepare_inputs(x, 2)
-
-        second_block = x[1::2]
-        check = split_x[:, x.size(1):, :]
-
-        self.assertTrue((second_block == check).all())
-
-        unsplit_x = model._split_outputs(split_x, 2)
-
-        self.assertTrue((x == unsplit_x).all())
+        # self.assertTrue(out.size(0) == 23)
+        # self.assertTrue(out.size(1) == 3)
+        # self.assertTrue(out.size(2) == 10)
 
 
-class TestHidden(unittest.TestCase):
-    def test(self):
-        model = drnn.DRNN(10, 10, 4, 0, 'GRU')
-
-        x = torch.randn(23, 3, 10)
-
-        hidden = model(x)[1]
-
-        self.assertEqual(len(hidden), 4)
-
-        for hid in hidden:
-            print(hid.size())
-
-
-class TestPassHidden(unittest.TestCase):
-    def test(self):
-        model = drnn.DRNN(10, 10, 4, 0, 'GRU')
-
-        hidden = []
-        for i in range(4):
-            hidden.append(torch.randn(2 ** i, 3, 10))
-
-        x = torch.randn(24, 3, 10)
-        hidden = model(x, hidden)
-
+# class TestReshape(unittest.TestCase):
+#     def test(self):
+#         model = drnn.DRNN(10, 10, 4, 0, 'GRU')
+#
+#         x = torch.randn(24, 3, 10)
+#
+#         split_x = model._prepare_inputs(x, 2)
+#
+#         second_block = x[1::2]
+#         check = split_x[:, x.size(1):, :]
+#
+#         self.assertTrue((second_block == check).all())
+#
+#         unsplit_x = model._split_outputs(split_x, 2)
+#
+#         self.assertTrue((x == unsplit_x).all())
+#
+#
+# class TestHidden(unittest.TestCase):
+#     def test(self):
+#         model = drnn.DRNN(10, 10, 4, 0, 'GRU')
+#
+#         x = torch.randn(23, 3, 10)
+#
+#         hidden = model(x)[1]
+#
+#         self.assertEqual(len(hidden), 4)
+#
+#         for hid in hidden:
+#             print(hid.size())
+#
+#
+# class TestPassHidden(unittest.TestCase):
+#     def test(self):
+#         model = drnn.DRNN(10, 10, 4, 0, 'GRU')
+#
+#         hidden = []
+#         for i in range(4):
+#             hidden.append(torch.randn(2 ** i, 3, 10))
+#
+#         x = torch.randn(24, 3, 10)
+#         hidden = model(x, hidden)
+#
 
 if __name__ == '__main__':
     unittest.main()
